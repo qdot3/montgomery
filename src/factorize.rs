da8c1e270a39bd29f5ac898bfbe24db22bd7a6b4
qdot3 <quantum.engine.55g@gmail.com>
2026-04-11 23:30:06 +0900

    modify

--- src/factorize.rs
+++ src/factorize.rs
@@ -6,9 +6,9 @@ use crate::{prime::primality_test, Context64};
 static SMALL_ODD_PRIME_CONTEXT_16: [(u16, u64, u16); 6541] =
     include!("./small_prime_context_u16_raw.rs");
 
-/// Factorize integer and write prime factors in `factor` in any order.
+/// Factorize integer and writes prime factors to `factor` in any order.
 ///
-/// This function may fail.
+/// This function is probabilistic and may fail.
 ///
 /// # Time complexity
 ///
@@ -86,7 +86,7 @@ fn pollard_rho(x: u64) -> Option<NonZero<u64>> {
 
         let mut prod = one;
         let mut step = 0;
-        let mut memo = [[0, 0, one.value]; 32];
+        let mut memo = [[0, 0, one.value]; 1 << 5];
 
         'a: while !prod.is_zero() {
             y0.value = f(y0.value);
@@ -134,6 +134,33 @@ fn pollard_rho(x: u64) -> Option<NonZero<u64>> {
                 }
             }
         }
+
+        for i in 0..(step % (1 << 10)) >> 5 {
+            let g = binary_gcd(memo[i][2], x);
+
+            if g != 1 {
+                if primality_test(g) {
+                    return NonZero::new(g);
+                }
+
+                y0.value = memo[i][0];
+                y1.value = memo[i][1];
+                for _ in 0..1 << 5 {
+                    let g = binary_gcd((y0 - y1).value, x);
+
+                    if g != 1 {
+                        if primality_test(g) {
+                            return NonZero::new(g);
+                        } else {
+                            return pollard_rho(g);
+                        }
+                    }
+
+                    y0.value = f(y0.value);
+                    y1.value = f(f(y1.value));
+                }
+            }
+        }
     }
 
     None
@@ -162,61 +189,25 @@ fn binary_gcd(mut a: u64, mut b: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use rand::{random_iter, rng, Rng};
+    use rand::{rng, seq::SliceRandom, Rng};
 
     use super::*;
-
-    fn base(mut primes: Vec<u64>) {
-        assert!(
-            primes.iter().all(|p| primality_test(*p)),
-            "only prime numbers are accepted"
-        );
-
-        let mut factor = Vec::new();
-        assert!(factorize(
-            primes
-                .iter()
-                .fold(1, |prod, p| prod.checked_mul(*p).unwrap()),
-            &mut factor,
-        )
-        .is_ok());
-
-        factor.sort_unstable();
-        primes.sort_unstable();
-        assert_eq!(factor, primes)
-    }
-
-    #[test]
-    fn large1() {
-        base(vec![998_244_353, 1_000_000_007]);
-    }
-
-    #[test]
-    fn large2() {
-        base(vec![982_451_629, 982_451_653]);
-    }
-
-    #[test]
-    fn large_square() {
-        for p in [998_244_353, 1_000_000_007] {
-            base(vec![p; 2]);
-        }
-    }
-
-    #[test]
-    fn random_small() {
-        for n in random_iter::<u32>().take(10_000) {
-            let mut factor = Vec::new();
-
-            assert!(factorize(n as u64, &mut factor).is_ok());
-            assert_eq!(n as u64, factor.iter().product())
-        }
-    }
 
     #[test]
     fn random_square() {
         let mut rng = rng();
-        for n in std::iter::repeat_with(|| rng.random_range(1 << 20..1 << 30)).take(30) {
+        for n in std::iter::repeat_with(|| rng.random_range(1 << 20..1 << 32)).take(5000) {
+            let mut factor = Vec::new();
+
+            assert!(factorize(n * n, &mut factor).is_ok());
+            assert_eq!(n * n, factor.iter().product())
+        }
+    }
+
+    #[test]
+    fn random_cube() {
+        let mut rng = rng();
+        for n in std::iter::repeat_with(|| rng.random_range(1 << 16..1 << 21)).take(5000) {
             let mut factor = Vec::new();
 
             assert!(factorize(n * n, &mut factor).is_ok());
@@ -230,7 +221,7 @@ mod tests {
             .rev()
             .step_by(2)
             .filter(|n| primality_test(*n))
-            .take(100)
+            .take(500)
         {
             let mut factor = Vec::new();
 
@@ -241,17 +232,55 @@ mod tests {
 
     // fast since p is relatively small
     #[test]
-    fn cube() {
+    fn prime_cube() {
         let p = Vec::from_iter(
             (0..1 << 21)
                 .rev()
                 .step_by(2)
                 .filter(|n| primality_test(*n))
-                .take(300),
+                .take(500),
         );
 
         for p in p {
             let n = p.pow(3);
+            let mut factor = Vec::new();
+
+            assert!(factorize(n, &mut factor).is_ok());
+            assert_eq!(n, factor.iter().product())
+        }
+    }
+
+    #[test]
+    fn prime_double() {
+        let mut p: Vec<u64> = (0..1 << 32)
+            .rev()
+            .step_by(2)
+            .filter(|n| primality_test(*n))
+            .take(500)
+            .collect();
+        p.shuffle(&mut rng());
+
+        for p in p.windows(2) {
+            let n = p[0] * p[1];
+            let mut factor = Vec::new();
+
+            assert!(factorize(n, &mut factor).is_ok());
+            assert_eq!(n, factor.iter().product())
+        }
+    }
+
+    #[test]
+    fn prime_triple() {
+        let mut p: Vec<u64> = (0..1 << 21)
+            .rev()
+            .step_by(2)
+            .filter(|n| primality_test(*n))
+            .take(500)
+            .collect();
+        p.shuffle(&mut rng());
+
+        for p in p.windows(3) {
+            let n = p[0] * p[1] * p[2];
             let mut factor = Vec::new();
 
             assert!(factorize(n, &mut factor).is_ok());
